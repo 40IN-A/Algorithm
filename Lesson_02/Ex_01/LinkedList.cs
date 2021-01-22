c6248def2b3dcbdd0e5758d3ebacf39391cd62ea
Fert <ya.nibelungen@ya.ru>
2021-01-22 18:30:53 +0300

    Домашнее задание к уроку №4. Доделать не успел, постараюсь выполнить за выходные(комечу сейчас, ибо ссылка закроется через 20 минут).

---
+++ Lesson_02/Ex_01/LinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+
+    public interface ILinkedList
+    {
+        int GetCount(); //возращает количество элементов в списке
+
+        void AddNode(int value); //добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); //добавляет новый элемент списка после определенного элемента
+        void RemoveNode(int index); //удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); //улаляет указанный элемент
+        Node FindNode(int serchValue); //ищет элемент по его значению
+    }
+    public class LinkedList : ILinkedList
+    {
+        private int _count = 0;
+        private Node _startNode;
+        private Node _endNode;
+        public void AddNode(int value)
+        {
+            Node node = new Node(value);
+
+            if (_startNode == null)
+                _startNode = node;
+            else
+            {
+                _endNode.NextNode = node;
+                node.NextNode = _endNode;
+
+            }
+            _endNode = node;
+            _count++;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node FindNode(int serchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lesson_02/Ex_02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_02
+{
+    class Program
+    {
+        static int BinarrySearch(int[] array, int target)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                //Console.WriteLine($"{array[mid]}");
+                if (target == array[mid])
+                {
+                    return array[mid];          // Асимптотическая сложность данного алгоритма равна O(log N).
+                }                               
+                else if (target > array[mid])   
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            //Console.WriteLine("Ne popal");
+            return -1; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int target = Convert.ToInt32(Console.ReadLine());
+            int[] array = { 1, 2, 3, 15, 24, 55, 77 };
+            int answer = BinarrySearch(array, target);
+            if ( answer == -1 )
+            {
+                Console.WriteLine($"Введенное число: {target} в массиве отсутствует.");
+            }
+            else
+            {
+                Console.WriteLine($"Введенное число: {target} в массиве присутствует.");
+            }
+        }
+    }
+}
